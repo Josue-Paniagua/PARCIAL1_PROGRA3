b043038aaf8edf72905d8abc954777e5ff05c19d
Josue Paniagua <jpaniaguao@miumg.edu.gt>
2026-03-13 20:47:05 -0600

    Prueba de commit en casa

--- Parcial 1 serie 1/src/main/java/ejercicio1_serie1/ejercicio1.java
+++ Parcial 1 serie 1/src/main/java/ejercicio1_serie1/ejercicio1.java
@@ -2,16 +2,7 @@ package ejercicio1_serie1;
 
 public class ejercicio1 {
 	public static void main(String[] arg) {
-		int [] numeros = {1,2,3,4,5};
-		int total = 0;
-		for ( int i = 0; i<numeros.length; i++) {
-			total += score //"nombre la fucnion de abajo (numeros[i]);
-		}
-		System.out.println("total " + total);	
-	}
-	
-	public static int score(int number) {
-		
+		System.out.println("commit de lsito ");
 	}
 	
 
