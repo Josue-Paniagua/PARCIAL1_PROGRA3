39b010fd506f5866dded4e8f0b3276e1693cb2d9
Josue Paniagua <jpaniaguao@miumg.edu.gt>
2026-03-14 14:20:29 -0600

    Avance pruba 1 ejer 1 terminado

--- Parcial 1 serie 1/src/main/java/ejercicio1_serie1/ejercicio1.java
+++ Parcial 1 serie 1/src/main/java/ejercicio1_serie1/ejercicio1.java
@@ -6,6 +6,27 @@ public class ejercicio1 {
 		int total;
 		total = 0; 
 		
+		for (int i =0; i<numeros.length; i++) {
+			
+			total +=funcion(numeros[i]);
+					
+			System.out.println("El total es : " + total );
+		}
+		
+	}
+	
+	public static int funcion(int num) {
+	if (num ==0) {
+		return 0;}
+	if (num ==5) {
+		return 5;
+	}
+	if ( num %2 == 0) {
+		return 1;
+	}
+	return 3;
+	
+	}
 	
 	
 
