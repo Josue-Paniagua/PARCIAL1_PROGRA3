198706ce847e801992a54ee0c180cda68c72a737
Josue Paniagua <jpaniaguao@miumg.edu.gt>
2026-03-14 14:45:19 -0600

    avance

--- Parcial 1 serie 1/src/main/java/ejercicio1_serie1/Problema_2.java
+++ Parcial 1 serie 1/src/main/java/ejercicio1_serie1/Problema_2.java
@@ -2,9 +2,20 @@ package ejercicio1_serie1;
 
 public class Problema_2 {
 
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	 public static void main(String[] args) {
 
-	}
+	        int[] numeros = {7, 2, 9, 4, 1, 6};
 
-}
+	        int[] resultado = secondMinMax(numeros);
+
+	        System.out.println("Segundp mnor: " + resultado[0]);
+	        System.out.println("Segundo mayor: " + resultado[1]);
+	    }
+
+
+	    public static int[] secondMinMax(int[] numbers) {
+	    }
+	
+
+
